c5420282ec414e1c1c947c4b816d7897316b1498
Sebp23 <57201896+Sebp23@users.noreply.github.com>
2020-12-02 20:46:28 -0800

    Made greybox for map
    Need to find a way to scale down the greybox

--- QuakeDev/working/compile.bat
+++ QuakeDev/working/compile.bat
@@ -5,25 +5,25 @@ cd Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\working
 
 
 echo Copying Files...
-copy Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps\testlevel.map Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\working
+copy Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps\corruptrealm.map Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\working
 
 
 echo Converting map...
 
 
 echo --------------QBSP--------------
-Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\tools\ericw-tools\bin\qbsp.exe testlevel
+Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\tools\ericw-tools\bin\qbsp.exe corruptrealm
 
 echo --------------VIS---------------
-Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\tools\ericw-tools\bin\vis.exe testlevel
+Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\tools\ericw-tools\bin\vis.exe corruptrealm
 
 echo -------------LIGHT--------------
-Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\tools\ericw-tools\bin\light.exe testlevel
+Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\tools\ericw-tools\bin\light.exe corruptrealm
 
-copy testlevel.bsp Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps
-copy testlevel.pts Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps
-copy testlevel.lit Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps
+copy corruptrealm.bsp Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps
+copy corruptrealm.pts Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps
+copy corruptrealm.lit Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps
 pause
 cd\
 cd Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev
-quakespasm  +map testlevel
+quakespasm  +map corruptrealm
