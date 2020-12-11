8659566ac74b47eef3ea2d80a125d0ee8d0fd18e
Sebp23 <57201896+Sebp23@users.noreply.github.com>
2020-12-10 17:11:19 -0800

    made some minor changes to area 1 size. also made some combat encounter changes

--- QuakeDev/working/compile.bat
+++ QuakeDev/working/compile.bat
@@ -5,25 +5,25 @@ cd Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\working
 
 
 echo Copying Files...
-copy Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps\corruptrealmoriginal.map Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\working
+copy Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps\corruptrealmfinal.map Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\working
 
 
 echo Converting map...
 
 
 echo --------------QBSP--------------
-Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\tools\ericw-tools\bin\qbsp.exe corruptrealmoriginal
+Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\tools\ericw-tools\bin\qbsp.exe corruptrealmfinal
 
 echo --------------VIS---------------
-Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\tools\ericw-tools\bin\vis.exe corruptrealmoriginal
+Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\tools\ericw-tools\bin\vis.exe corruptrealmfinal
 
 echo -------------LIGHT--------------
-Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\tools\ericw-tools\bin\light.exe corruptrealmoriginal
+Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\tools\ericw-tools\bin\light.exe corruptrealmfinal
 
-copy corruptrealmoriginal.bsp Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps
-copy corruptrealmoriginal.pts Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps
-copy corruptrealmoriginal.lit Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps
+copy corruptrealmfinal.bsp Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps
+copy corruptrealmfinal.pts Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps
+copy corruptrealmfinal.lit Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps
 pause
 cd\
 cd Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev
-quakespasm  +map corruptrealmoriginal
+quakespasm  +map corruptrealmfinal
