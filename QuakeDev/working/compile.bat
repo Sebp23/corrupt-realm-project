0369a34a537cb1ed043dcae6e0b1936b8bbe0eda
Sebp23 <57201896+Sebp23@users.noreply.github.com>
2020-12-19 16:16:51 -0800

    renamed files and added screenshots

--- QuakeDev/working/compile.bat
+++ QuakeDev/working/compile.bat
@@ -5,24 +5,24 @@ cd Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\working
 
 
 echo Copying Files...
-copy Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps\corruptrealmfinal.map Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\working
+copy Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps\corruptrealm.map Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\working
 
 
 echo Converting map...
 
 
 echo --------------QBSP--------------
-Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\tools\ericw-tools\bin\qbsp.exe corruptrealmfinal
+Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\tools\ericw-tools\bin\qbsp.exe corruptrealm
 
 echo --------------VIS---------------
-Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\tools\ericw-tools\bin\vis.exe corruptrealmfinal
+Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\tools\ericw-tools\bin\vis.exe corruptrealm
 
 echo -------------LIGHT--------------
-Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\tools\ericw-tools\bin\light.exe corruptrealmfinal
+Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\tools\ericw-tools\bin\light.exe corruptrealm
 
-copy corruptrealmfinal.bsp Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps
-copy corruptrealmfinal.pts Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps
-copy corruptrealmfinal.lit Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps
+copy corruptrealm.bsp Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps
+copy corruptrealm.pts Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps
+copy corruptrealm.lit Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev\id1\maps
 cd\
 cd Q:\Workspace\GitHub\corrupt-realm-project\QuakeDev
-quakespasm  +map corruptrealmfinal
+quakespasm  +map corruptrealm
